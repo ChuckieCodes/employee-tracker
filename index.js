0eb8f7398c581ded31da332fba462821fd19bd15
ChuckieCodes <carlosmariopalacio@gmail.com>
2024-02-07 01:51:02 +0800

    add & show department

--- index.js
+++ index.js
@@ -10,3 +10,78 @@ const pool = mysql.createPool({
   database: 'employee_db',
   connectionLimit: 10,
 });
+
+async function queryDepartments() {
+  try {
+    const [rows] = await pool.execute(
+      'select * from department order by name'
+    )
+    return rows;
+  } catch(err) {
+    console.log(`Something went wrong: ${err}`);
+  }
+}
+
+async function showAllDepartments() {
+  console.log(`\nDepartment Table\n`)
+  console.table(await queryDepartments())
+  init();
+}
+
+async function addDepartment() {
+  inquirer
+    .prompt(
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter department name: ',
+      }
+    )
+    .then(async function({ name }) {
+      try {
+        const [result] = await pool.execute('insert into department (name) values (?)', [name]);
+        console.log(`\nInsert successful. Added ${name} in department table.\n`);
+        init();
+      } catch (err) {
+        console.log(`Something went wrong: ${err}`);
+      }
+    })
+}
+
+function init() {
+  console.log(`\nEmployee Management System\n`);
+  
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'task',
+      message: 'Would you like to do?',
+      choices: [
+        'Add Department',
+        'Show All Departments',
+        'Add Role',
+        'Update Role',
+        'Show All Roles',
+        'Add Employee',
+        'Update Employee',
+        'Show All Employees',
+        'Quit',
+      ],
+    })
+    .then(function ({ task }) {
+      switch (task) {
+        case 'Add Department':
+          addDepartment();
+          break;
+        case 'Show All Departments':
+          showAllDepartments();
+          break;
+        case 'Quit':
+          console.log(`\nbye\n`)
+          pool.end();
+        break;
+      }
+    });
+}
+
+init();
